9389f0ba93c794256c2a9e88a229ed1da98e8fdf
Melvin63 <melvinjones632004@gmail.com>
2023-12-17 09:36:22 +0530

    form validation done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { Validate } from "../utils/Validate";
 
 const Login = () => {
   const [isSignedIn, setisSignedIn] = useState(true);
+  const [isMessage, setisMessage] = useState(null);
+
+  const errorMessage = isMessage;
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const validationCheck = () => {
+    const message = Validate(email.current.value, password.current.value);
+    setisMessage(message);
+  };
 
   const isSignInCheck = () => {
     setisSignedIn(!isSignedIn);
   };
-
-  console.log(isSignedIn);
 
   return (
     <div className="relative">
@@ -18,9 +28,14 @@ const Login = () => {
         src="https://assets.nflxext.com/ffe/siteui/vlv3/ca6a7616-0acb-4bc5-be25-c4deef0419a7/c5af601a-6657-4531-8f82-22e629a3795e/IN-en-20231211-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
         alt=""
       />
-      <form className="absolute top-32  left-1/2 transform -translate-x-1/2 flex flex-col items-center bg-black bg-opacity-80 w-3/12  p-6  mb-auto">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="absolute top-32  left-1/2 transform -translate-x-1/2 flex flex-col items-center bg-black bg-opacity-80 w-3/12  p-6 "
+      >
         <h1 className=" font-bold text-3xl text-white mr-auto pb-2 pt-3">
-          {isSignedIn ? "Sign In" : "Sign out"}
+          {isSignedIn ? "Sign In" : "Sign Up"}
         </h1>
 
         {!isSignedIn && (
@@ -32,18 +47,30 @@ const Login = () => {
         )}
 
         <input
+          ref={email}
           type="email"
           placeholder="Email"
           className="p-2 m-3 w-full bg-gray-700 text-gray-100 rounded-sm  "
         />
+        {errorMessage === "Email is not valid" ? (
+          <p className="text-red-500 p-1 mr-auto">{errorMessage}</p>
+        ) : null}
 
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 m-3 w-full  bg-gray-700  text-gray-100 rounded-sm"
         />
-        <button className="bg-red-600 rounded text-white   py-2 m-3 w-full pb-2">
-          {isSignedIn ? "Sign In" : "Sign Out"}
+        {errorMessage === "Password is not valid" ? (
+          <p className="text-red-500 p-1 mr-auto">{errorMessage}</p>
+        ) : null}
+
+        <button
+          onClick={validationCheck}
+          className="bg-red-600 rounded text-white   py-2 m-3 w-full pb-2"
+        >
+          {isSignedIn ? "Sign In" : "Sign Up"}
         </button>
         <p
           onClick={isSignInCheck}
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ6O5UmEQuXfQqMWoyLLDe8mTip1-g1dc",
+  authDomain: "netflix-gpt-d3690.firebaseapp.com",
+  projectId: "netflix-gpt-d3690",
+  storageBucket: "netflix-gpt-d3690.appspot.com",
+  messagingSenderId: "107221660096",
+  appId: "1:107221660096:web:cc10d62fbcc83846a25825",
+  measurementId: "G-W2PV5HZ68K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
